15301bfbcfb1fcd242b50998e4ec167425813b7a
Satanand Burla <satanand.burla@gmail.com>
2019-12-20 02:17:30 -0800

    changes

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ echo $MYPATH
 cd ~
 mkdir -p ~/bin
 ln -s $MYPATH/bin/* ~/bin/
-ln -s $MYPATH/.dircolors
+#ln -s $MYPATH/.dircolors
 ln -s $MYPATH/gitconfig_office .gitconfig
 #ln -s $MYPATH/.mailcap
-ln -s $MYPATH/.offlineimaprc
+#ln -s $MYPATH/.offlineimaprc
 ln -s $MYPATH/vim/.vim
 ln -s $MYPATH/vim/.vim/.vimrc
 ln -s $MYPATH/.tmux.conf
