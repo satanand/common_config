098dad1769f10217531db8b1aca9b5bdda7bac86
Satanand Burla <satanand.burla@gmail.com>
2020-11-02 02:44:20 +0530

    fix setup
    Signed-off-by: Satanand Burla <satanand.burla@gmail.com>

--- setup.sh
+++ setup.sh
@@ -4,12 +4,7 @@ echo $MYPATH
 cd ~
 mkdir -p ~/bin
 ln -s $MYPATH/bin/* ~/bin/
-#ln -s $MYPATH/.dircolors
-ln -s $MYPATH/gitconfig_office .gitconfig
-#ln -s $MYPATH/.mailcap
-#ln -s $MYPATH/.offlineimaprc
+ln -s $MYPATH/gitconfig_personal .gitconfig
 ln -s $MYPATH/vim/.vim
 ln -s $MYPATH/vim/.vim/.vimrc
 ln -s $MYPATH/.tmux.conf
-#mkdir -p ~/.mutt
-#cp -r $MYPATH/mutt/* ~/.mutt/
